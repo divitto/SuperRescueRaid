ca7feec50c65f48840eb6302f764c9a5738b7506
Anthony Divitto <anthonydivitto@gmail.com>
2018-10-24 21:48:51 -0700

    initial upload of SuperRescueRaid
    First upload of Super Rescue Raid my first game made using a 2d game engine based on what you can learn from the SFML Game Development text book. This version is an almost complete alpha version with only a few minor additions to add before it can be advanced to a beta version.

---
+++ Game/Category.lua
@@ -0,0 +1,13 @@
+return {
+    None                = "00000000";
+    SceneAirLayer	= "00000001";
+    PlayerAircraft	= "00000010";
+    Tree        	= "00000100";
+    EnemyAircraft	= "00001000";
+    Pickup		= "00010000";
+    AlliedProjectile	= "00100000";
+    EnemyProjectile	= "01000000";
+
+    Aircraft            = "00011100";
+    Projectile          = "11000000";
+};
---
+++ Game/SpawnPoints.lua
@@ -0,0 +1,24 @@
+local a = require( "Game/Resources/Aircrafts" ).Map;
+
+-- When you change this to match everything else make whatever handles spawning also give the craft
+-- a directions vector that way one plane type can have multiple patterns
+
+
+--addEnemy( a.Hellcat,          200,  800.0 );
+--addEnemy( a.Hellcat,         -200,  900.0 );
+--addEnemy( a.GreenTurret,     -100, 1000.0 );
+--addEnemy( a.GreenTurret,      100, 1000.0 );
+--addEnemy( a.GreenTurret,     -250, 1200.0 );
+--addEnemy( a.GreenTurret,      250, 1200.0 );
+--addEnemy( a.Hellcat,          200, 1200.0 );
+--addEnemy( a.Hellcat,         -200, 1200.0 );
+
+--addEnemy( a.GreenTurret,     -100, 1300.0 );
+--addEnemy( a.GreenTank,       70.0, 1600.0 );
+--addEnemy( a.GreenTurret,      100, 1300.0 );
+--addEnemy( a.GreenTank,      -70.0, 1400.0 );
+--addEnemy( a.GreenTank,      -70.0, 1600.0 );
+--addEnemy( a.GreenTank,       70.0, 1400.0 );
+--addEnemy( a.Hellcat,          100, 1800.0 );
+--addEnemy( a.Hellcat,         -100, 1800.0 );
+
